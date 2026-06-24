357c08ee36c8af59c6b010ac22cbe313fcd09fe0
BAGAsg121 <shlokgoswami58@gmail.com>
2026-06-24 12:01:59 +0530

    Add: valid-palindrome-ii - submission-0

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-0.java
@@ -0,0 +1,55 @@
+class Solution {
+
+        
+
+    public boolean validPalindrome(String s) {
+
+        int n = s.length();
+        int i = 0;
+        int j= n-1;
+
+        String strs = s.toLowerCase().replaceAll("[^a-z0-9]", "");
+
+        char[] chars = strs.toCharArray();
+
+
+        while (i<j){
+
+            if (chars[i] != chars[j]){
+
+                return isPalindrome(s, i +1, j) || isPalindrome(s, i, j-1);
+            }
+            i++;
+            j--;
+        
+        }   return true;
+
+
+    }
+
+
+    public boolean isPalindrome(String s, int i, int j){
+        
+        String strs = s.toLowerCase().replaceAll("[^a-z0-9]", "");
+
+        char[] chars = strs.toCharArray();
+
+        while (i<j){
+
+            if (chars[i] != chars[j]){
+                return false;
+            }
+            i++;
+            j--;
+        } 
+        return true;
+
+    }
+
+
+
+
+
+
+}
+
